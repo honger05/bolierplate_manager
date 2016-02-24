341b330c41df3688bc875a35c872d557913dda76
honger05 <honger05@126.com>
2016-02-24 14:28:09 +0800

    add bui

--- gallery/z/seed.js
+++ gallery/z/seed.js
@@ -1,9 +1,14 @@
 
-
+require('./assets/less/dpl.less')
+require('./assets/less/bui.less')
 
 var Z = require('./common/common')
 
+// base
 require('./data/base')
+
+// ui
+require('./overlay/base')
 
 
 module.exports = Z
